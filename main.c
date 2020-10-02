6b6376185d6bc015ba0079d6da903b7ffaf7e742
jd199 <joshuadonald@lcwc.ac.uk>
2020-10-02 09:58:51 +0000

    restored file

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+#include "stdio.h"
+
+int main(void) {
+  int firstNumber, secondNumber, sum;
+  printf("Enter the first number:");
+  scanf("%d", &firstNumber);
+  printf("Enter the second number:");
+  scanf("%d", &secondNumber);
+  sum = firstNumber + secondNumber;
+  printf("The sum of %d and %d is %d.", firstNumber, secondNumber, sum);
+  return 0;
 }
